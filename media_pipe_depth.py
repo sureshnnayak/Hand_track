91ca8c0445a35b99c16bc9e490690735a0351b33
sureshnnayak <sureshnnayak24@gmail.com>
2022-06-10 17:07:17 -0600

    odo pyhton code

--- media_pipe_depth.py
+++ media_pipe_depth.py
@@ -14,9 +14,30 @@ import cv2
 import numpy as np
 import datetime as dt
 import imutils
+import rospy
+from nav_msgs.msg import Odometry
+from std_msgs.msg import String
+from tf.transformations import euler_from_quaternion
+from hand import Hand
+
+import json
 
 
-
+def odometryCb(msg):
+    global camera
+    x = msg.pose.pose.position.x
+    y = msg.pose.pose.position.y
+    z = msg.pose.pose.position.z
+    quaternion = msg.pose.pose.orientation
+    orientation_list = [quaternion.x, quaternion.y, quaternion.z, quaternion.w]
+    orientation = euler_from_quaternion(orientation_list)
+    if camera is None:
+        print("Camera located!")
+        camera = Hand(x, y, z, orientation)
+        # print(camera)
+    else:
+        camera.update_pose(x, y, z, orientation)
+    
 
 #======================================Target Object detection================
 # define names of each possible ArUco tag OpenCV supports
@@ -43,7 +64,7 @@ ARUCO_DICT = {
 	"DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
 	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
 }
-def detect(image,type="DICT_5X5_100"):
+def detect(image,dimentions, type="DICT_5X5_100"):
     #resize the image
     #image = imutils.resize(image, width=600)
     # verify that the supplied ArUCo tag exists and is supported by
@@ -51,49 +72,16 @@ def detect(image,type="DICT_5X5_100"):
 
     # load the ArUCo dictionary, grab the ArUCo parameters, and detect
     # the markers
-    #print("[INFO] detecting '{}' tags...".format(type))
     arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[type])
     arucoParams = cv2.aruco.DetectorParameters_create()
     (corners, ids, rejected) = cv2.aruco.detectMarkers(image, arucoDict,
         parameters=arucoParams)
-    print(len(corners))
-    """if len(corners) > 0:
-        
-        # flatten the ArUco IDs list
-        ids = ids.flatten()
-        for (markerCorner, markerID) in zip(corners, ids):
-                # extract the marker corners (which are always returned in
-                # top-left, top-right, bottom-right, and bottom-left order)
-                corners = markerCorner.reshape((4, 2))
-                (topLeft, topRight, bottomRight, bottomLeft) = corners
-                # convert each of the (x, y)-coordinate pairs to integers
-                topRight = (int(topRight[0]), int(topRight[1]))
-                bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
-                bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
-                topLeft = (int(topLeft[0]), int(topLeft[1]))
+    y_len, x_len,z_len = dimentions
+    x_len -=2
+    y_len -=2
 
-
-        # draw the bounding box of the ArUCo detection
-                cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
-                cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
-                cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
-                cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
-                # compute and draw the center (x, y)-coordinates of the ArUco
-                # marker
-                cX = int((topLeft[0] + bottomRight[0]) / 2.0)
-                cY = int((topLeft[1] + bottomRight[1]) / 2.0)
-                cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
-                # draw the ArUco marker ID on the image
-                cv2.putText(image, str(markerID),
-                    (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.5, (0, 255, 0), 2)
-                print("[INFO] ArUco marker ID: {}".format(markerID))
-                # show the output image
-    cv2.imshow("Image", image)
-    #cv2.waitKey(0)
-    """
     if len(corners) > 0:
-        print(len(corners))
+        #print(len(corners))
         ids = ids.flatten()
         # loop over the detected ArUCo corners
         for (markerCorner, markerID) in zip(corners, ids):
@@ -101,25 +89,16 @@ def detect(image,type="DICT_5X5_100"):
             corners = markerCorner.reshape((4, 2))
             (topLeft, topRight, bottomRight, bottomLeft) = corners
             # convert each of the (x, y)-coordinate pairs to integers
-            topRight = (int(topRight[0]), int(topRight[1]))
-            bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
-            bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
-            topLeft = (int(topLeft[0]), int(topLeft[1]))
-    
-            
-            
-#============
-# =========
+
+            topRight = ( x_len - int(topRight[0]),  int(topRight[1]))
+            bottomRight = (x_len -int(bottomRight[0]), int(bottomRight[1]))
+            bottomLeft = (x_len -int(bottomLeft[0]), int(bottomLeft[1]))
+            topLeft = (x_len - int(topLeft[0]), int(topLeft[1]))
+
             return topRight, bottomRight, bottomLeft,topLeft
         #return cX,cY #only last object is returned in this case. if there are multiple objects we need to create a list 
     return -1, -1, -1, -1
 #======================================Target Object detection================
-
-#======================================ROS====================================
-
-import rospy
-from std_msgs.msg import String
-#======================================ROS====================================
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 org = (20, 100)
@@ -154,10 +133,17 @@ for i in range(len(realsense_ctx.devices)):
     detected_camera = realsense_ctx.devices[i].get_info(rs.camera_info.serial_number)
     print(f"{detected_camera}")
     connected_devices.append(detected_camera)
-device = connected_devices[0] # In this example we are only using one camera
-pipeline = rs.pipeline()
-config = rs.config()
+print(connected_devices)
+#device = connected_devices[0] # use this when we are only using one camera
+device_rgb = '115422250228' #Hardcoding the device Number 
+pipeline_rgb = rs.pipeline()
+config_rgb = rs.config()
 background_removed_color = 153 # Grey
+
+device_odo = '952322110941'
+pipeline_odo = rs.pipeline()
+config_odo = rs.config()
+
 
 # ====== Mediapipe ======
 mpHands = mp.solutions.hands
@@ -166,7 +152,8 @@ mpDraw = mp.solutions.drawing_utils
 
 
 # ====== Enable Streams ======
-config.enable_device(device)
+config_rgb.enable_device(device_rgb)
+config_odo.enable_device(device_odo)
 
 # # For worse FPS, but better resolution:
 # stream_res_x = 1280
@@ -177,45 +164,76 @@ stream_res_y = 480
 
 stream_fps = 30
 
-config.enable_stream(rs.stream.depth, stream_res_x, stream_res_y, rs.format.z16, stream_fps)
-config.enable_stream(rs.stream.color, stream_res_x, stream_res_y, rs.format.bgr8, stream_fps)
-profile = pipeline.start(config)
+config_rgb.enable_stream(rs.stream.depth, stream_res_x, stream_res_y, rs.format.z16, stream_fps)
+config_rgb.enable_stream(rs.stream.color, stream_res_x, stream_res_y, rs.format.bgr8, stream_fps)
+profile = pipeline_rgb.start(config_rgb)
 
+pipeline_odo.start(config_odo)
 align_to = rs.stream.color
 align = rs.align(align_to)
 
 # ====== Get depth Scale ======
 depth_sensor = profile.get_device().first_depth_sensor()
 depth_scale = depth_sensor.get_depth_scale()
-print(f"\tDepth Scale for Camera SN {device} is: {depth_scale}")
+print("Depth:")
+print(depth_scale)
+print(f"\tDepth Scale for Camera SN {device_rgb} is: {depth_scale}")
 
-# ====== Set clipping distance ======
-clipping_distance_in_meters = 2
-clipping_distance = clipping_distance_in_meters / depth_scale
-print(f"\tConfiguration Successful for SN {device}")
 
 # ====== Get and process images ====== 
-print(f"Starting to capture images on SN: {device}")
+print(f"Starting to capture images on SN: {device_rgb}")
 
 
 #======================================ROS====================================
-pub = rospy.Publisher('Hand', String, queue_size=10)
+pub = rospy.Publisher('hand_and_target', String, queue_size=10)
 rospy.init_node('talker', anonymous=True)
 rate = rospy.Rate(10) # 10hz
 
+
+
+rospy.Subscriber('/camera/odom/sample', Odometry, odometryCb)
+camera = None
+
+Object_detect_count = 0
 #while True:
 while not rospy.is_shutdown():
 
     #======================================ROS====================================
-
+    hand_info = None
+    target_info = None
+    number_of_hands = 0
     start_time = dt.datetime.today().timestamp()
+    
+    odo_frames = pipeline_odo.wait_for_frames()
+    
+
+        # Fetch pose frame
+    odo_pose = odo_frames.get_pose_frame()
+    if odo_pose:
+            # Print some of the pose data to the terminal
+            data = odo_pose.get_pose_data()
+            #print("Frame #{}".format(odo_pose.frame_number))
+            #print("Position: {}".format(data.translation))
+            #print("Velocity: {}".format(data.velocity))
+    
+    pri = 1
+    if pri:
+        print(odo_pose)
+        pri = 0
 
     # Get and align frames
-    frames = pipeline.wait_for_frames()
+    frames = pipeline_rgb.wait_for_frames()
     aligned_frames = align.process(frames)
     aligned_depth_frame = aligned_frames.get_depth_frame()
     color_frame = aligned_frames.get_color_frame()
-    
+
+    #get odometry data
+    odometry_data = None
+    if camera:
+        camera_x, camera_y, camera_z = camera.get_pose()
+        roll, pitch, yaw = camera.orientation
+        odometry_data = '"odometry":[{0},{1},{2},{3},{4},{5}]'.format(camera_x, camera_y, camera_z,roll, pitch, yaw)
+        
     if not aligned_depth_frame or not color_frame:
         continue
 
@@ -223,33 +241,19 @@ while not rospy.is_shutdown():
     depth_image = np.asanyarray(aligned_depth_frame.get_data())
     depth_image_flipped = cv2.flip(depth_image,1)
     color_image = np.asanyarray(color_frame.get_data())
-    #detect target
-    #color_frame    
-    topRight, bottomRight, bottomLeft,topLeft = detect(color_image)
 
-    #topRight, bottomRight, bottomLeft,topLeft = detect( cv2.flip(color_image,1))
+    dimensions = color_image.shape
     
-    depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #Depth image is 1 channel, while color image is 3
-    background_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), background_removed_color, color_image)
-
-    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-    images = background_removed
-    #images = cv2.flip(background_removed,1)
-    #color_image = cv2.flip(color_image,1)
-    
-
-    
+    topRight, bottomRight, bottomLeft, topLeft = detect(color_image, dimensions)
+    #images = color_image
+    color_image = cv2.flip(color_image,1)
+    images = color_image
 
     color_images_rgb = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
 
     # Process hands
     results = hands.process(color_images_rgb)
-    #=============================================target detection======================================
-    #topRight, bottomRight, bottomLeft,topLeft = detect(color_images_rgb)
-    
-    #target_x, target_y = detect(color_images_rgb)
 
-    
     if results.multi_hand_landmarks:
         number_of_hands = len(results.multi_hand_landmarks)
         i=0
@@ -265,30 +269,63 @@ while not rospy.is_shutdown():
                 x = len(depth_image_flipped[0]) - 1
             if y >= len(depth_image_flipped):
                 y = len(depth_image_flipped) - 1
-            mfk_distance = depth_image_flipped[y,x] * depth_scale # meters
-            mfk_distance_feet = mfk_distance * 3.281 # feet
-            images = cv2.putText(images, f"{hand_side} Hand Distance: {mfk_distance_feet:0.3} feet ({mfk_distance:0.3} m) away", org2, font, fontScale, color, thickness, cv2.LINE_AA)
+            
+            hand_z = depth_image_flipped[y,x] * depth_scale # meters
+            #mfk_distance_feet = hand_z * 3.281 # feet
+            images = cv2.putText(images, f"{hand_side} Hand Distance: {hand_z:0.3} m away", org2, font, fontScale, color, thickness, cv2.LINE_AA)
             #print("depth:", mfk_distance)
             i+=1
             
             #======================================ROS==================================== 
-            if hand_side =="Right" and topLeft != -1 :
-           
-                target_x = int((topLeft[0] + bottomRight[0]) / 2.0)
-                target_y = int((topLeft[1] + bottomRight[1]) / 2.0)
-                target_x_meter = ((target_x - (REALSENSE_PPX)) / REALSENSE_FX) * mfk_distance #rel_positions[i][1]
-                target_y_meter = ((target_y - (REALSENSE_PPY)) / REALSENSE_FY) * mfk_distance #rel_positions[i][1]
-                x_meter = ((x - (REALSENSE_PPX)) / REALSENSE_FX) * mfk_distance #rel_positions[i][1]
-                y_meter = ((y - (REALSENSE_PPY)) / REALSENSE_FY) * mfk_distance #rel_positions[i][1]
 
-                coordinates = "z(depth):"+ str(mfk_distance) +",\tx:" + str(x_meter)+"\ty:" + str(y_meter) + "\tTarget_x" + str(target_x_meter)+ "\ttarget_y:" + str(target_y_meter)
-                #rospy.loginfo(coordinates)
-                pub.publish(coordinates)
+
+            if hand_side =="Right"  :
+                x_meter = ((x - (REALSENSE_PPX)) / REALSENSE_FX) * hand_z #rel_positions[i][1]
+                y_meter = ((y - (REALSENSE_PPY)) / REALSENSE_FY) * hand_z #rel_positions[i][1]
+                
+                hand_info= '"hand":[{0},{1},{2}]'.format(x_meter, y_meter, hand_z)
+                
+    if topLeft != -1:#target
+       
+        target_x = int((topLeft[0] + bottomRight[0]) / 2.0)
+        target_y = int((topLeft[1] + bottomRight[1]) / 2.0)
+        target_z = depth_image_flipped[target_y,target_x] * depth_scale # meters
+        #print(target_z, depth_image_flipped[target_x,target_y] * depth_scale )# meters)
+
+        #target_z = depth_image[target_y,target_x] * depth_scale # meters
+        target_x_meter = ((target_x - (REALSENSE_PPX)) / REALSENSE_FX) * target_z #rel_positions[i][1]
+        target_y_meter = ((target_y - (REALSENSE_PPY)) / REALSENSE_FY) * target_z #rel_positions[i][1]
+        
+        target_info = '"target":[{0},{1},{2}]'.format(target_x_meter,target_y_meter,target_z)
+        #target_info = json.dumps(target_info)
+        #st_l = '{'
+        #st_r = '}'
+        #target_info = st_l + target_info + st_r
+        org2 = (20, org[1]+(20*(5+1)))
+        images = cv2.putText(images, f"Target Distance: {target_z:0.3} m away", org2, font, fontScale, color, thickness, cv2.LINE_AA)
+        
+
+        #"My name is {0}, I'm {1}".format("John",36)
+        #hand_info = "z(depth):"+ str(mfk_distance) +",\tx:" + str(x_meter)+"\ty:" + str(y_meter) 
+        #target_info = "\tTarget_x" + str(target_x_meter)+ "\ttarget_y:" + str(target_y_meter)
+    ros_msg = None
+    comma= ','
+    if hand_info and target_info:
+        ros_msg = hand_info +comma+ target_info 
+       
+    elif hand_info:
+        ros_msg = hand_info 
+        
+    elif target_info:
+        ros_msg =  target_info 
+   
+    if odometry_data and ros_msg:
+
+        ros_msg = ros_msg + comma + odometry_data
+        pub.publish(ros_msg)
+
             #======================================ROS====================================
 
-        images = cv2.putText(images, f"Hands: {number_of_hands}", org, font, fontScale, color, thickness, cv2.LINE_AA)
-    else:
-        images = cv2.putText(images,"No Hands", org, font, fontScale, color, thickness, cv2.LINE_AA)
 
 
     # Display FPS
@@ -297,15 +334,19 @@ while not rospy.is_shutdown():
     org3 = (20, org[1] + 60)
     images = cv2.putText(images, f"FPS: {fps}", org3, font, fontScale, color, thickness, cv2.LINE_AA)
 
-    name_of_window = 'SN: ' + str(device)
+    name_of_window = 'SN: ' + str(device_rgb)
 
-    # ======================adding Bounding Box to target image==================================================
+    # ====================== adding Bounding Box to target image==================================================
     if topLeft != -1:
+        if Object_detect_count < 100 :
+            Object_detect_count = 0
+        else :
+            Object_detect_count +=1
         # draw the bounding box of the ArUCo detection
-        cv2.line(images, topLeft, topRight, (0, 255, 0), 2)
-        cv2.line(images, topRight, bottomRight, (0, 255, 0), 2)
-        cv2.line(images, bottomRight, bottomLeft, (0, 255, 0), 2)
-        cv2.line(images, bottomLeft, topLeft, (0, 255, 0), 2)
+        cv2.line(images, topLeft, topRight, (255, 0, 0), 2)
+        cv2.line(images, topRight, bottomRight, (255, 0, 0), 2)
+        cv2.line(images, bottomRight, bottomLeft, (255, 0, 0), 2)
+        cv2.line(images, bottomLeft, topLeft, (255, 0, 0), 2) # Red 
     
     # show the output image
     # Display images 
@@ -314,9 +355,13 @@ while not rospy.is_shutdown():
     key = cv2.waitKey(1)
     # Press esc or 'q' to close the image window
     if key & 0xFF == ord('q') or key == 27:
-        print(f"User pressed break key for SN: {device}")
+        print(f"User pressed break key for SN: {device_rgb}")
         break
     
 print(f"Application Closing")
-pipeline.stop()
+pipeline_rgb.stop()
 print(f"Application Closed.")
+
+
+#if __name__ == "__main__":
+    
